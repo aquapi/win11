91617f0792e09f122ebdb7c8742e862ae37109d9
Revenity <87530663+aquapi@users.noreply.github.com>
2022-08-14 23:02:01 +0700

    Log all web vitals

--- src/index.js
+++ src/index.js
@@ -12,9 +12,12 @@ root.render(
 	</React.StrictMode>
 );
 
+// If you don't want to use service worker,
+// delete public/sw.js, utils/swLoader.js and the register() call below
+// Learn more about service workers: https://cra.link/PWA
 register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
